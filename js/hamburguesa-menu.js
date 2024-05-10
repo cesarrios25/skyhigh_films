376ca263d6dea6f64f6ab558def33f90c2e2b66f
cesarrios25 <cesar.riosg25@gmail.com>
2024-05-10 16:55:46 -0500

    proyecto final frontend terminado

--- js/hamburguesa-menu.js
+++
@@ -1,4 +0,0 @@
-function menuHamburguesa() {
-    let menuMobile = document.getElementById('menuMobile').value;
-        menuMobile.style.display = menuMobile.style.display === 'block' ? 'none' : 'block';
-    }
---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('hamburger-menu').addEventListener('click', () => {
+    document.querySelector('.container-right').classList.toggle('active');
+});
